bbb35d0f9e1feeaef04f3af2a0e8ec58b2d6638b
ryun-dev@gmail.com <ryuni.dev@gmail.com>
2022-05-22 22:15:07 +0900

    중간 커밋

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const { PORT, MONGO_URI } = process.env;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Successfully connected to mongodb'))
+  .catch(e => console.error(e));
+
+app.listen(PORT, () => {
+	console.log(`listening on ${PORT}`);
+});
